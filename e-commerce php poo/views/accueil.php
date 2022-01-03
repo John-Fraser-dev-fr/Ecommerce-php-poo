d594d60ab24e1b621b9c532d973f113938fe49b5
John Fraser <burton_fsr@icloud.com>
2022-01-03 12:12:55 +0100

    font-end/description produits

--- e-commerce php poo/views/accueil.php
+++ e-commerce php poo/views/accueil.php
@@ -90,21 +90,14 @@
         <div class="card  m-3 col-3 products">
             
             <div class="card-body product">
-            <img src="assets/image_produits/<?= $article['modele'] ?>.jpg" class="img-fluid" >
+            <a href="index.php?controller=article&task=show&id=<?= $article['id_article'] ?>"><img src="assets/image_produits/<?= $article['modele'] ?>.jpg" class="img-fluid" ></a>
                 <p class="card-text-modele"> <?= $article['modele'] ?></p>
                 <p class="card-text-detail"><?= $article['detail_modele'] ?></p>
                 <p class="card-text-prix"> <?= number_format($article['prix'], 2, ',', ' ') ?> €</p>
             </div>
-            <a href="index.php?controller=article&task=show&id=<?= $article['id_article'] ?>">description</a>
+            
             <div class="card-action">
-            <form method="POST" action="index.php?controller=panier&task=add&id=<?= $article['id_article'] ?>">
-
-            <input type="submit" class="btn btn-primary" name="ajout_panier" value="Ajouter au panier"></input>
-            <input type="hidden" name="id_article"  value="<?php echo $article['id_article']; ?>" >
-     
-                   
-                
-            </form>
+            
 </div>
         </div>
 
--- e-commerce php poo/views/description.php
+++ e-commerce php poo/views/description.php
@@ -1,23 +1,32 @@
 
 
 
-<h1><?= $article['marque'] ?></h1>
+<div class="row"> 
 
-<p><?= $article['modele'] ?></p>
+    <div>
+        <img src="assets/image_produits/<?= $article['modele'] ?>.jpg" class="img-fluid" >
+    </div>
 
-<p><?= number_format($article['prix'], 2, ',', ' ') ?> €</p>
+    <div>
+        <p><b><?= $article['marque'] ?></b></p>
+        <p><?= $article['modele'] ?></p>
+        <p><?= number_format($article['prix'], 2, ',', ' ') ?> €</p>
+        <div class="card-action row">
+            <form method="POST" action="index.php?controller=panier&task=add&id=<?= $article['id_article'] ?>">
+                <input type="submit" class="btn btn-primary" name="ajout_panier" value="Ajouter au panier"></input>
+                <input type="hidden" name="id_article"  value="<?php echo $article['id_article']; ?>" >
+            </form>
+        </div>
+    </div>
 
-
-<div class="card-action row">
-<form method="POST" action="index.php?controller=panier&task=add&id=<?= $article['id_article'] ?>">
-
-<input type="submit" class="btn btn-primary" name="ajout_panier" value="Ajouter au panier"></input>
-<input type="hidden" name="id_article"  value="<?php echo $article['id_article']; ?>" >
-
-       
-    
-</form>
 </div>
+
+
+
+
+
+
+
 
 
 
