b2c74ee517425b01677bc71800668bec605865ba
John Fraser <burton_fsr@icloud.com>
2022-01-13 15:30:03 +0100

    finalisation achat

--- e-commerce php poo/views/finalisation.php
+++ e-commerce php poo/views/finalisation.php
@@ -48,7 +48,7 @@
           
 
     <div class="col-6">
-        <div class="accordion" id="accordionExample">
+        <div class="accordion" id="accordion1">
             <div class="accordion-item">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne" style="display:grid">
                     <h5 class="accordion-header" id="headingOne">Mes informations</h5><br>
@@ -58,10 +58,29 @@
                 
                 <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample" >
                     <div class="accordion-body">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        <form method="POST" >
+                            <div class="form-group">
+                                <label>Nom</label>
+                                <input type="text" class="form_finalisation form-control" value="<?php echo $_SESSION['nom'];?>" name="nom" >
+                            </div>
+                            <div class="form-group">
+                                <label>Prénom</label>
+                                <input type="text" class="form_finalisation form-control" value="<?php echo $_SESSION['prenom'];?>" name="prenom">
+                            </div>
+                            <div class="form-group">
+                                <label >Email </label>
+                                <input type="email" class="form_finalisation form-control" value="<?php echo $_SESSION['email'];?>" name="email">
+                            </div>
+                            <div class="d-grid gap-2" style="text-align: -webkit-center;">
+                                <button type="submit" class="btn  btn-primary">Enregistrer</button>
+                                <a href="index.php?controller=user&task=finalisation" ><button class="btn  btn-primary" type="button">Annuler</button></a>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="accordion" id="accordion2">
             <div class="accordion-item">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style="display:grid">
                     <h5 class="accordion-header" id="headingTwo">Adresse de livraison</h5><br>
@@ -72,49 +91,65 @@
                 </button>
                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample" >
                     <div class="accordion-body">
-                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                    </div>
-                </div>
-            </div>
-            <div class="accordion-item">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style="display:grid">
-                    <h5 class="accordion-header" id="headingThree">Détails de votre commande</h5><br>
-                    <?php if(array_sum($_SESSION['panier']['qte_produit']) == 1) {?>
-                    <p class="info_accordeon">1 article</p>
-                    <?php }else{?>
-                    <p class="info_accordeon"><?php echo array_sum($_SESSION['panier']['qte_produit']);?> articles</p>
-                    <?php } ?>
-                </button>
-                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample" >
-                    <div class="accordion-body">
-                        <div>
-                            <?php for ($i=0 ;$i < count($_SESSION['panier']['modele']) ; $i++){ ?>
-                            <div>
-                                <img src="assets/image_produits/<?= $_SESSION['panier']['modele'][$i] ?>.jpg" class="img-fluid" style="width:60px; height: auto;">
+                    <form method="POST" >
+                            <div class="form-group">
+                                <label>Numéro</label>
+                                <input type="text" class="form_finalisation form-control" value="<?php echo $_SESSION['numero_rue'];?>" name="numero_rue" >
                             </div>
-                            <div class="pop_panier_description col-6">
-                                <p style="margin-bottom: 0"><b><?= htmlspecialchars($_SESSION['panier']['modele'][$i])?></b></p>
-                                <p style="margin-bottom: 0"><?= number_format($_SESSION['panier']['prix'][$i], 2, ',','')?> € </p>
-                                <p style="font-size: 12px">Quantité : <?= htmlspecialchars($_SESSION['panier']['qte_produit'][$i])?> </p>
+                            <div class="form-group">
+                                <label>Rue</label>
+                                <input type="text" class="form_finalisation form-control" value="<?php echo $_SESSION['rue'];?>" name="rue">
                             </div>
-                            <?php } ?>
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
-            <div class="accordion-item">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour" style="display:grid">
-                    <h5 class="accordion-header" id="headingFour">Paiement</h5><br>
-                    <p class="info_accordeon">...</p>
-                </button>
-                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample" >
-                    <div class="accordion-body">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            <div class="form-group">
+                                <label >Code postal</label>
+                                <input type="email" class="form_finalisation form-control" value="<?php echo $_SESSION['code_postal'];?>" name="code_postal">
+                            </div>
+                            <div class="form-group">
+                                <label >Ville</label>
+                                <input type="email" class="form_finalisation form-control" value="<?php echo $_SESSION['ville'];?>" name="ville">
+                            </div>
+                            <div class="form-group">
+                                <label >Pays</label>
+                                <input type="email" class="form_finalisation form-control" value="<?php echo $_SESSION['pays'];?>" name="pays">
+                            </div>
+                            <div class="d-grid gap-2" style="text-align: -webkit-center;">
+                                <button type="submit" class="btn  btn-primary">Enregistrer</button>
+                                <a href="index.php?controller=user&task=finalisation" ><button class="btn  btn-primary" type="button">Annuler</button></a>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="details_commande">
+            <h5>Détails de votre commande</h5><br>
+            <?php if(array_sum($_SESSION['panier']['qte_produit']) == 1) {?>
+            <p class="info_accordeon">1 article</p>
+            <?php }else{?>
+            <p class="info_accordeon"><?php echo array_sum($_SESSION['panier']['qte_produit']);?> articles</p>
+            <?php } ?>
+       
+            <div class="details_commande_produits">
+                <?php for ($i=0 ;$i < count($_SESSION['panier']['modele']) ; $i++){ ?>
+                <div class="commande_produit">
+                    <div class="img_produit">
+                        <img src="assets/image_produits/<?= $_SESSION['panier']['modele'][$i] ?>.jpg" class="img-fluid" style="width:100px; height: 100px">
+                    </div>
+                    <div class="details_commande_description">
+                    <p style="line-height: 1.5;font-size: 0.6rem; text-align:center; margin-bottom: 0.2rem;"><b><?= htmlspecialchars($_SESSION['panier']['modele'][$i])?> </b></p>
+                        <p style="line-height: 1.5;font-size: 0.5rem; text-align:center">Quantité : <?= htmlspecialchars($_SESSION['panier']['qte_produit'][$i])?> </p>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+                        
+        <div class="detail_paiement">
+            <h5>Paiement</h5><br>
+            <p class="info_accordeon">...</p>
+        </div>
+        
     </div>      
 
     <div class="col-2">
